46db09c765a09eafa924499446f3e0ffd1b13e6c
AdsWilar <wilarads@gmail.com>
2023-06-04 14:24:35 -0400

    Initial commit

---
+++ src/main/java/bo/jads/myfinancesbackend/MyFinancesBackendApplication.java
@@ -0,0 +1,13 @@
+package bo.jads.myfinancesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinancesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinancesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/jads/myfinancesbackend/MyFinancesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.jads.myfinancesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
